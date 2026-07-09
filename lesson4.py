3dda27d1ea5f9890de087a95b6f4ea90fadcc0da
isoojiife <adeade742@gmail.com>
2026-07-09 17:50:23 +0100

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,14 @@
+#activity1 
+f1 = 150
+f2 = 98
+f3 = 120
+f4 = 92
+f5 = 110
+total = f1 + f2 + f3 + f4 + f5
+avg = total / 5
+print (total)
+print (avg)
+bags = total/25
+leftover = total%25
+print (bags)
+print (leftover)
