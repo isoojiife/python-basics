9cee9e659fd023efe80fae60b39376d95b3f0e0e
isoojiife <adeade742@gmail.com>
2026-07-27 17:45:34 +0100

    Add files via upload

---
+++ lesson12.py
@@ -0,0 +1,28 @@
+#activity 1
+v = 6
+w = 4
+x = 8
+y = 3
+z = 2
+z = (v + w) * (x - y)
+print ("value of (v+w) * x/ y is ", z)
+
+
+
+#activity 2
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+if num1 % num2 == 0:
+    print(num1, "is divisible by", num2)
+else:
+    print(num1, "is not divisible by", num2)
+
+
+
+#activity 3
+mean = 38
+total = 40
+sum = mean * total
+sum = sum - 36 + 56
+correct_mean = sum / total
+print ("The correct mean is", correct_mean)
