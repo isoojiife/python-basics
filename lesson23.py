f6fb86072dbf985f8610513bce000f798b824399
isoojiife <adeade742@gmail.com>
2026-08-19 18:41:17 +0100

    Add files via upload

---
+++ lesson23.py
@@ -0,0 +1,10 @@
+#activity1
+for row in range(1,6):
+    for column in range(1, row+1):
+        print("*", end="")
+    print()
+#activity2
+print("enter the total number of rows")
+rows = int(input())
+for i in range(1, rows+1):
+    
